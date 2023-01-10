5a00a717a26ba049b92433ec8d5052cd90e45980
exCR0SS <MailboxofficialPetrovAA@gmail.com>
2023-01-11 01:11:55 +0500

    подправил

--- Example15.py
+++ Example15.py
@@ -6,4 +6,4 @@ class Main:
     userNum1 = int(input("Введите 1 число: "))
     userNum2 = int(input("Введите 2 число: "))
 
-    print(userNum1 + userNum2, userNum1 - userNum2 if userNum1 > userNum2 else userNum2 - userNum1)
+    print(f"Сумма: {userNum1 + userNum2}, разность: {userNum1 - userNum2}")
